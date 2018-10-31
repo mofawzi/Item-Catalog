c0076307fbab166fdae3f071a728a36e673ec833
Mohamed Fawzy <mofawzy1996@gmail.com>
2018-10-31 04:31:02 +0200

    Add CRUD functionality

--- project.py
+++ project.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python2.7
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
+# Database imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+# Use the Flask framework
 app = Flask(__name__)
+
+# Create the database engine
+engine = create_engine('sqlite:///restaurantsmenu.db?check_same_thread=False')
+Base.metadata.bind = engine
+
+# Create the database session to apply CRUD
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 # Get all restaurants route
@@ -12,7 +26,9 @@ app = Flask(__name__)
 def showRestaurants():
     """This method should retrieve all restaurants
     from the database and show them to the users"""
-    return render_template('restaurants.html')
+
+    restaurants = session.query(Restaurant)
+    return render_template('restaurants.html', restaurants=restaurants)
 
 
 # Create restaurant route
@@ -22,7 +38,16 @@ def showRestaurants():
 def newRestaurant():
     """This method should Create a new restaurant
     and save it to the database"""
-    return render_template('newRestaurant.html')
+
+    if request.method == 'POST':
+        newRestaurant = Restaurant(
+            name=request.form['name'])
+
+        session.add(newRestaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newRestaurant.html')
 
 
 # Edit restaurant route
@@ -32,7 +57,19 @@ def newRestaurant():
 def editRestaurant(restaurant_id):
     """This method should Update or Edit a restaurant
     and save the changes to the database"""
-    return render_template('editRestaurant.html')
+
+    editedRestaurant = session.query(Restaurant).filter_by(
+        id=restaurant_id).one()
+
+    if request.method == 'POST':
+        if request.form['name']:
+            editedRestaurant.name = request.form['name']
+        session.add(editedRestaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template(
+            'editRestaurant.html', restaurant=editedRestaurant)
 
 
 # Delete restaurant route
@@ -41,7 +78,17 @@ def editRestaurant(restaurant_id):
     methods=['GET', 'POST', 'Delete'])
 def deleteRestaurant(restaurant_id):
     """This method should Delete a restaurant from the database"""
-    return render_template('deleteRestaurant.html')
+
+    deletedRestaurant = session.query(Restaurant).filter_by(
+        id=restaurant_id).one()
+
+    if request.method == 'POST':
+        session.delete(deletedRestaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template(
+            'deleteRestaurant.html', restaurant=deletedRestaurant)
 
 
 # Show the menu of a restaurant route
@@ -54,7 +101,14 @@ def deleteRestaurant(restaurant_id):
 def showMenu(restaurant_id):
     """This method should retrieve all the menu items
     of a restaurants from the database and show them to the users"""
-    return render_template('menu.html')
+
+    restaurant = session.query(Restaurant).filter_by(
+        id=restaurant_id).one()
+
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+
+    return render_template('menu.html', items=items, restaurant=restaurant)
 
 
 # Create new menu item route
@@ -64,7 +118,24 @@ def showMenu(restaurant_id):
 def newMenuItem(restaurant_id):
     """This method should Create a new menu item for a restaurant
     and save it to the database"""
-    return render_template('newMenu.html')
+
+    restaurant = session.query(Restaurant).filter_by(
+        id=restaurant_id).one()
+
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name=request.form['name'],
+            price=request.form['price'],
+            description=request.form['description'],
+            course=request.form['course'],
+            restaurant_id=restaurant_id)
+
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'newMenuItem.html', restaurant=restaurant)
 
 
 # Edit a menu item route
@@ -74,7 +145,26 @@ def newMenuItem(restaurant_id):
 def editMenuItem(restaurant_id, menu_id):
     """This method should Update or Edit a menu item of a restaurant
     and save the changes to the database"""
-    return render_template('editMenu.html')
+
+    restaurant = session.query(Restaurant).filter_by(
+        id=restaurant_id).one()
+
+    editedItem = session.query(MenuItem).filter_by(
+        id=menu_id).one()
+
+    if request.method == 'POST':
+        editedItem = MenuItem(
+            name=request.form['name'],
+            price=request.form['price'],
+            description=request.form['description'],
+            course=request.form['course'])
+
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'editMenuItem.html', restaurant=restaurant, item=editedItem)
 
 
 # Delete a menu item route
@@ -84,7 +174,20 @@ def editMenuItem(restaurant_id, menu_id):
 def deleteMenuItem(restaurant_id, menu_id):
     """This method should Delete a menu item of a restaurant
     from the database"""
-    return render_template('deleteMenu.html')
+
+    restaurant = session.query(Restaurant).filter_by(
+        id=restaurant_id).one()
+
+    deletedItem = session.query(MenuItem).filter_by(
+        id=menu_id).one()
+
+    if request.method == 'POST':
+        session.delete(deletedItem)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'deleteMenuItem.html', restaurant=restaurant, item=deletedItem)
 
 
 if __name__ == '__main__':
